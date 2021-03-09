8dde68f3b94a33157c01fc9e2f3ff423a9db5d9c
Renato Benatti <rbenatti@uolinc.com>
2021-03-09 06:36:37 -0300

    Remoção código desnecessário

--- config/settings.php
+++ config/settings.php
@@ -6,18 +6,11 @@ return function (ContainerBuilder $containerBuilder) {
 
     $containerBuilder->addDefinitions([
         'settings' => [
-            'component' => 'vegas-ws',
+            'component' => '',
             'localEnvironment' => getenv('DOCKER_CONTAINER') === '1',
             'logger' => [
                 'colorized'     => (bool)getenv('DOCKER_CONTAINER'),
                 'customLogPath' => '/tmp/logfile'
-            ],
-            'externalComponents' => [
-                'refund' => getenv('REFUND_URI'),
-                'fallout' => getenv('FALLOUT_URI'),
-                'vegasCheckout' => getenv('VEGAS_CHECKOUT_URI'),
-                'falloutExternalUrl' => getenv('FALLOUT_EXTERNAL_URL'),
-                'vegasCheckoutExternalUrl' => getenv('VEGAS_CHECKOUT_EXTERNAL_URL')
             ],
             'mysql' => [
                 'host'      => getenv('DATABASE_HOST'),
